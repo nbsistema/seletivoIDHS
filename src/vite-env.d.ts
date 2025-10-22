696f17fd460d40d46d6d8b8c5a8542c4a2683804
nbsistema <nbconsultoriasistema@gmail.com>
2025-10-21 21:50:57 -0300

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
-/// <reference types="vite/client" />
+VITE_GOOGLE_CLIENT_ID=7946337108-ugfh3hq2goea4ujd12ronkh1u1cmjpbm.apps.googleusercontent.com
+VITE_GOOGLE_API_KEY=AIzaSyAepavxoe3uYwe6KRZ_RJdCmFS5DmgNjuY
+VITE_GOOGLE_SHEETS_ID=https:d/1NaetcGUJ5_HYsQ-NCK3V3zFEnDfyfwmjX4wrUwI7NFw/edit?usp=sharing
+
+
+
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const API_KEY   = import.meta.env.VITE_GOOGLE_API_KEY;
+const SHEET_ID  = import.meta.env.VITE_GOOGLE_SHEETS_ID;
+
