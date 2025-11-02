ff6089d738e25ce7b6ade51f034659fe13cb9997
nbsistema <nbconsultoriasistema@gmail.com>
2025-11-02 19:13:43 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    plugins: [react()],
+    optimizeDeps: {
+        exclude: ['lucide-react']
+    },
+    server: {
+        port: 3000, // Adicione uma porta fixa
+        host: true, // Permite acesso externo se necessário
+    }
 });
